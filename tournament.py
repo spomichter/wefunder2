c83a52dd2fd42c9dcde16b0f278dea7d4ae23518
stashpomichter <pomichterstash@gmail.com>
2019-02-24 16:08:07 -0500

    finished most of tourney backend

--- tournament.py
+++ tournament.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 class Match:
     person1 = ''
@@ -20,17 +21,68 @@ class Match:
             return self.person2
 
 
-
 class tournamentRunner:
     players = []
+
     def __init__(self):
-        players = []
+
         self.main()
 
     def main(self):
+        for x in range(17):
+            self.players.append("player"+str(x))
+
+        print(self.players)
+        currentwinners = []
+        r = 1
+        while (len(currentwinners)) > 1:
+            R = round(r, self.players)
+            currentwinners = (R.run())
 
 
 
+class round:
+    players = []
+    winners = []
+    roundnum = 0
+    def __init__(self,num, players):
+        self.roundnum = num
+        self.players = players
+
+
+    def run(self):
+
+        if self.power2(len(self.players)) == True:
+            for i in range(0, len(self.players), 2):
+                m = Match(self.players[i], self.players[i+1])
+                self.winners.append(m.getWinner())
+
+        else: #if not a power of two
+            freepasses = self.floor_log(len(self.players))
+            print(str(freepasses) + " players got a free pass")
+            for i in range(freepasses):
+                self.winners.append(self.players[i])
+                self.players.pop(i)
+
+            for i in range(0, len(self.players), 2):
+                m = Match(self.players[i], self.players[i+1])
+                self.winners.append(m.getWinner())
+
+        return(self.winners)
+
+    def power2(self, num):
+        return num != 0 and ((num & (num - 1)) == 0)
+
+    def floor_log(self,num):
+        if num == 0:
+            return 0
+        return num - (2 ** int(math.log(num, 2)))
+
+
+
+k = tournamentRunner()
+#m = round(1, ['1324', 'asdf'])
+#print(m.floor_log(11))
 
 
 
